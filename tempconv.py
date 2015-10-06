ef12e291bc6ad98f931832181139ec7b6e973cda
jteske54 <teske.jacob@gmail.com>
2015-10-06 13:39:05 -0500

    First push of final tempconv.py

--- tempconv.py
+++ tempconv.py
@@ -1,16 +1,49 @@
-def cTof():
+def incorrect():
+    print("You have selected an incorrect number. Please select again.")
 
+def tempConvChoose():
+    print("""
+    1. Kelvin
+    2. Celsius
+    3. Fahrenheit
+    """)
+    tempChoice = int(input("What would you like to convert? "))
+    if tempChoice == 1:
+        kelvin()
+    elif tempChoice == 2:
+        celcius()
+    elif tempChoice == 3:
+        fahrenheit()
+    else:
+        incorrect()
+        input()
+        tempConvChoose()
 
-def cTok():
+def kelvin():
+    k = int(input("K "))
+    c = k - 273
+    f = (c * 1.8) + 32
+    print("K= " + k)
+    print("ºC= " + c)
+    print("ºF= " + f)
+    return k
 
+def celcius():
+    c = int(input("ºC "))
+    k = c + 273
+    f = (c * 1.8) + 32
+    print("K= " + k)
+    print("ºC= " + c)
+    print("ºF= " + f)
+    return k
 
-def fTok():
+def fahrenheit():
+    f = int(input("ºF "))
+    c = (f - 32) / 1.8
+    k = c + 273
+    print("K= " + k)
+    print("ºC= " + c)
+    print("ºF= " + f)
+    return k
 
-
-def fToc():
-
-
-def kTof():
-
-
-def kToc():
+tempConvChoose()
